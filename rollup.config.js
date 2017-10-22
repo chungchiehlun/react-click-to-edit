2ca91fa4bc12446987240a4b0c920a69e5b21773
chungchiehlun <wuceh14678@gmail.com>
2017-10-22 14:54:18 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,67 +1,74 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import globals from 'rollup-plugin-node-globals'
-import resolve from 'rollup-plugin-node-resolve'
-import postcss from 'rollup-plugin-postcss'
+import spawn from "cross-spawn";
 
-import cssmodule from 'postcss-modules'
-import simplevars from 'postcss-simple-vars'
-import nested from 'postcss-nested'
-import cssnext from 'postcss-cssnext'
+import rollup from "rollup";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import globals from "rollup-plugin-node-globals";
+import resolve from "rollup-plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
 
-const generateScopedName = '[name]__[local]___[hash:base64:5]'
+import cssmodule from "postcss-modules";
+import cssnext from "postcss-cssnext";
+
+const generateScopedName = "[name]__[local]___[hash:base64:5]";
+const watchOptions = {};
+const watcher = rollup.watch({});
+
+watcher.on("event", event => {
+  if (event.code === "START") {
+    spawn("webpack-dev-server", { stdio: "inherit" });
+  }
+});
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    file: 'build/index.js',
-    format: 'cjs'
+    file: "build/index.js",
+    format: "cjs"
   },
-  external: ['react', 'react-dom'],
+  external: ["react", "react-dom"],
   plugins: [
     postcss({
       plugins: [
         cssmodule({
-          generateScopedName,
+          generateScopedName
         }),
-        simplevars(),
-        nested(),
         cssnext({
-          warnForDuplicates: false,
-        }),
+          warnForDuplicates: false
+        })
       ],
-      extensions: ['.css'],
+      extensions: [".css"]
     }),
     babel({
       babelrc: false,
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**",
       presets: [
         [
-          'env',
+          "env",
           {
-            modules: false,
+            modules: false
           }
         ],
-        'react',
+        "react"
       ],
       plugins: [
-        'transform-object-rest-spread',
-        'external-helpers',
+        "transform-object-rest-spread",
+        "external-helpers",
         [
-          'react-css-modules',
+          "react-css-modules",
           {
-            generateScopedName,
-          },
-        ],
+            generateScopedName
+          }
+        ]
       ]
     }),
     commonjs(),
     globals(),
     resolve({
       customResolveOptions: {
-        moduleDirectory: 'node_modules'
-      },
-    }),
+        moduleDirectory: "node_modules"
+      }
+    })
   ],
   sourcemap: true
-}
+};
