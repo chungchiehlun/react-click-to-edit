f199f678eddef7976eca0e75a9603fca56fe1a99
Jay Chung <wuceh14678@gmail.com>
2018-09-05 12:41:15 +0800

    add new pkg and config jest

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js"
   },
-  setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.js"
+  setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.js",
+  // https://jestjs.io/docs/en/configuration.html#snapshotserializers-array-string
+  snapshotSerializers: ["enzyme-to-json/serializer"]
 };
