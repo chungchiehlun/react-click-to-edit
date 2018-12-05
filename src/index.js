70eaf2d6198473e7061c8882193776e6e689a49e
[Ar][Te]m.[Be][Rn][At]skyy <artem.bernatskyy@gmail.com>
2018-12-05 02:05:50 +0200

    eslint compatible

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-import "./styles.css";
+import './styles.css';
 
 class ClickToEdit extends React.Component {
   constructor() {
     super();
     this.state = {
-      onEditMode: false
+      onEditMode: false,
     };
     this.input = React.createRef();
   }
   getIntoEditMode = () => {
     this.setState({
-      onEditMode: true
+      onEditMode: true,
     });
   };
   getOffEditMode = () => {
     this.setState({
-      onEditMode: false
+      onEditMode: false,
     });
     if (this.props.endEditing) {
       this.props.endEditing(this.input.current.value);
     }
   };
-  handleEnterKey = e => {
-    if (e.keyCode === 13 || e.charCode == 13) {
+  handleEnterKey = (e) => {
+    if (e.key === 'Enter') {
       this.setState({
-        onEditMode: false
+        onEditMode: false,
       });
       if (this.props.endEditing) {
         this.props.endEditing(this.input.current.value);
@@ -37,22 +37,24 @@ class ClickToEdit extends React.Component {
   };
   render() {
     return (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
       <section
-        className={classNames("CTE--wrapper", this.props.wrapperClass)}
+        className={classNames('CTE--wrapper', this.props.wrapperClass)}
         onClick={this.getIntoEditMode}
       >
         {this.state.onEditMode ? (
           <input
             type="text"
+            // eslint-disable-next-line jsx-a11y/no-autofocus
             autoFocus
             defaultValue={this.props.value}
-            className={classNames("CTE--input", this.props.inputClass)}
+            className={classNames('CTE--input', this.props.inputClass)}
             onKeyPress={this.handleEnterKey}
             onBlur={this.getOffEditMode}
             ref={this.input}
           />
         ) : (
-          <span className={classNames("CTE--text", this.props.textClass)}>
+          <span className={classNames('CTE--text', this.props.textClass)}>
             {this.props.value}
           </span>
         )}
@@ -66,13 +68,13 @@ ClickToEdit.propTypes = {
   inputClass: PropTypes.string,
   textClass: PropTypes.string,
   value: PropTypes.string.isRequired,
-  endEditing: PropTypes.func.isRequired
+  endEditing: PropTypes.func.isRequired,
 };
 
 ClickToEdit.defaultProps = {
-  wrapperClass: "",
-  inputClass: "",
-  textClass: ""
+  wrapperClass: '',
+  inputClass: '',
+  textClass: '',
 };
 
 export default ClickToEdit;
